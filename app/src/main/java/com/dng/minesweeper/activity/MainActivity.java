8221bab000897d65770a3703c9971db519d3b91d
damongeorgiou <dngx113@gmail.com>
2019-02-21 18:56:48 -0500

    create activity and fragment packages; move MainActivity.java into activity package

--- app/src/main/java/com/dng/minesweeper/activity/MainActivity.java
+++ app/src/main/java/com/dng/minesweeper/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dng.minesweeper;
+package com.dng.minesweeper.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dng.minesweeper.R;
 
 public class MainActivity extends AppCompatActivity {
 
