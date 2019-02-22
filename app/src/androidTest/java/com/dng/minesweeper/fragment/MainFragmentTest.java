65b7a2a9d62fec2c892fb5d949a8044fe647e009
damongeorgiou <dngx113@gmail.com>
2019-02-22 14:26:37 -0500

    write instrumentation test for MainFragment.java to assure it is properly launched

---
+++ app/src/androidTest/java/com/dng/minesweeper/fragment/MainFragmentTest.java
@@ -0,0 +1,59 @@
+package com.dng.minesweeper.fragment;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.dng.minesweeper.R;
+import com.dng.minesweeper.activity.MainActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
+
+public class MainFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private MainActivity mMainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mMainActivity = mMainActivityActivityTestRule.getActivity();
+    }
+
+    // Test if the fragment is successfully launched
+    @Test
+    public void testLaunch() {
+        // Test Container view is not null
+        FrameLayout frameLayout = mMainActivity.findViewById(R.id.activity_main_frameLayout);
+        assertNotNull(frameLayout);
+
+        // Test fragment vertical recycler view is not null
+        MainFragment mainFragment = MainFragment.newInstance(new HashMap<Integer, Integer>(), 0);
+
+        FragmentTransaction fragmentTransaction = mMainActivity.getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(), mainFragment);
+        fragmentTransaction.commit();
+
+        // waits for fragment to load before continuing
+        getInstrumentation().waitForIdleSync();
+
+        View view = mainFragment.getView().findViewById(R.id.fragment_main_vertRecyclerView);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mMainActivityActivityTestRule = null;
+        mMainActivity = null;
+    }
+}
