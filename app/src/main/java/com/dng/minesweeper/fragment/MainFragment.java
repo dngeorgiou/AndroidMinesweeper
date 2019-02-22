6050b46d6d1c3d77e2bcb9a3f222fae0009785cf
damongeorgiou <dngx113@gmail.com>
2019-02-21 19:53:45 -0500

    update MainFragment.java's factory methods/fields to better represent them

--- app/src/main/java/com/dng/minesweeper/fragment/MainFragment.java
+++ app/src/main/java/com/dng/minesweeper/fragment/MainFragment.java
@@ -10,25 +10,24 @@ import android.view.ViewGroup;
 
 import com.dng.minesweeper.R;
 
+import java.util.HashMap;
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
+ * {@link MainFragment.OnMainFragmentListener} interface
  * to handle interaction events.
  * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // the fragment initialization parameters
+    private static final String ARG_MAP = "map";
 
-    private OnFragmentInteractionListener mListener;
+    private String mMap;
+
+    private OnMainFragmentListener mListener;
 
     public MainFragment() {
         // Required empty public constructor
@@ -38,26 +37,33 @@ public class MainFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param map Parameter 1.
      * @return A new instance of fragment MainFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
+    public static MainFragment newInstance(HashMap<Integer, Integer> map) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ARG_MAP, map);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnMainFragmentListener) {
+            mListener = (OnMainFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnMainFragmentListener");
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mMap = getArguments().getString(ARG_MAP);
         }
     }
 
@@ -66,24 +72,6 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
@@ -102,8 +90,7 @@ public class MainFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public interface OnMainFragmentListener {
+        void onBlockPressed(int i);
     }
 }
