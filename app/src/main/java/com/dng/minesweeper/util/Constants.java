c4a29fab234a5d97c710eba1a39cae43c21af60e
damongeorgiou <dngx113@gmail.com>
2019-03-11 12:07:48 -0400

    create Constants.java; update with Highscore constants

---
+++ app/src/main/java/com/dng/minesweeper/util/Constants.java
@@ -0,0 +1,11 @@
+package com.dng.minesweeper.util;
+
+public class Constants {
+
+    class Highscore {
+        public static final String HIGHSCORES = "highscores";
+        public static final String NAME = "name";
+        public static final String SCORE = "score";
+    }
+
+}
