35a8c68b7387503a4d93fb5a6af3538201a25335
damongeorgiou <dngx113@gmail.com>
2019-03-11 12:48:21 -0400

    create HighscoesActivity.java

---
+++ app/src/main/java/com/dng/minesweeper/activity/HighscoresActivity.java
@@ -0,0 +1,31 @@
+package com.dng.minesweeper.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dng.minesweeper.R;
+import com.dng.minesweeper.fragment.HighscoresFragment;
+
+public class HighscoresActivity extends AppCompatActivity implements HighscoresFragment.OnHighscoresFragmentListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscores);
+
+        HighscoresFragment highscoresFragment = HighscoresFragment.newInstance();
+        setFragment(highscoresFragment);
+    }
+
+    /**
+     * Method takes in a fragment as a parameter and replaces MainActivity.java's container view with
+     * it.
+     */
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.activity_highscores_frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
