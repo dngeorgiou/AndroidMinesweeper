08594551d46910c77b6f03c6e7d4cc42a89ee1f3
damongeorgiou <dngx113@gmail.com>
2019-03-11 19:02:08 -0400

    create DirectionsActivity

---
+++ app/src/main/java/com/dng/minesweeper/activity/DirectionsActivity.java
@@ -0,0 +1,17 @@
+package com.dng.minesweeper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dng.minesweeper.R;
+
+public class DirectionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directions);
+    }
+
+    
+}
