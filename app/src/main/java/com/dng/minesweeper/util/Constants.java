b8944cc54fdfb521a784410fed9b74e2b8a869e5
damongeorgiou <dngx113@gmail.com>
2019-03-11 12:11:18 -0400

    fix Constants.java's subclass to be public

--- app/src/main/java/com/dng/minesweeper/util/Constants.java
+++ app/src/main/java/com/dng/minesweeper/util/Constants.java
@@ -2,7 +2,7 @@ package com.dng.minesweeper.util;
 
 public class Constants {
 
-    class Highscore {
+    public class Highscore {
         public static final String HIGHSCORES = "highscores";
         public static final String NAME = "name";
         public static final String SCORE = "score";
